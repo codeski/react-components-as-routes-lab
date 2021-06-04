7916499dd98e88eee26a55e674a0e77e9c7f0b1c
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-06-03 21:53:47 -0600

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,27 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  const renderActors = () => {
+    // console.log(directors)
+    return (  actors.map((actor, index) => { 
+          return (
+            <div key={index}>
+              <h3>Name: {actor.name}</h3>
+              <p>Movies:</p>
+              <ul>
+                {actor.movies.map((movie, index) => (
+                  <li key={index}>{movie}</li>
+                ) )}
+              </ul>
+            </div>
+          )
+      }
+    ))
+  }
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {renderActors()}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,28 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+
+  const renderDirectors = () => {
+    // console.log(directors)
+    return (  directors.map((director, index) => { 
+          return (
+            <div key={index}>
+              <h3>Name: {director.name}</h3>
+              <p>Movies:</p>
+              <ul>
+                {director.movies.map((movie, index) => (
+                  <li key={index}>{movie}</li>
+                ) )}
+              </ul>
+            </div>
+          )
+      }
+    ))
+  }
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {renderDirectors()}
     </div>
   );
 }
